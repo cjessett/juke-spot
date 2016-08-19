dbf771e784ef82631656184da647c021d74dcebe
CJ Jessett <cjessett@gmail.com>
2016-08-19 15:45:22 -0500

    update current user helper

--- app/helpers/spotify.rb
+++ app/helpers/spotify.rb
@@ -1,6 +1,8 @@
 helpers do
   def current_user
-    session[:spotify]['info'] = session[:spotify]['extra']['raw_info']
-    @current_user ||= RSpotify::User.new(session[:spotify])
+    if session[:spotify]
+      session[:spotify]['info'] = session[:spotify]['extra']['raw_info']
+      @current_user ||= RSpotify::User.new(session[:spotify])
+    end
   end
 end
