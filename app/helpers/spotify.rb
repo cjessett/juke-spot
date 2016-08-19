e7c8bf631a5f7804815ada5313ad4eb2b86a781b
tomhodev <datomster21@gmail.com>
2016-08-19 16:00:07 -0500

    add logo to layout

--- app/helpers/spotify.rb
+++ app/helpers/spotify.rb
@@ -1,6 +1,6 @@
 helpers do
   def current_user
-    session[:spotify]['info'] = session[:spotify]['extra']['raw_info']
-    @current_user ||= RSpotify::User.new(session[:spotify])
+    session[:spotify]['info'] = session[:spotify]['extra']['raw_info'] unless session[:spotify].nil?
+    @current_user ||= RSpotify::User.new(session[:spotify]) unless session[:spotify].nil?
   end
 end
